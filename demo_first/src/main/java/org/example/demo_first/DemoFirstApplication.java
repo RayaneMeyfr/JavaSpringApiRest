3e7dda7b24dd4c988d68ff21ff4d0dd462e53089
RayaneMeyfr <rayanemeyfr@hotmail.com>
2025-08-18 11:08:40 +0200

    first-commit

---
+++ demo_first/src/main/java/org/example/demo_first/DemoFirstApplication.java
@@ -0,0 +1,13 @@
+package org.example.demo_first;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoFirstApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoFirstApplication.class, args);
+    }
+
+}
